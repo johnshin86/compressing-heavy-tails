f5ee6e2de8058baedb40f97fe149b0f0d597e5b8
John Shin <johnyoungshin@gmail.com>
2021-03-24 12:45:04 -0400

    implemented get_data_svhn

--- compressing_ht.py
+++ compressing_ht.py
@@ -253,3 +253,63 @@ def get_data_cifar100(
 		num_workers=4,
 		shuffle=False)
 	return train_dataset, test_dataset, train_loader, test_loader
+
+def get_data_svhn(
+		train_batch_size=100,
+		test_batch_size=100,
+		train_range=None, 
+		test_range=None, 
+		random_labels=False, 
+		seed = 0):
+	"""Get CIFAR10 data. If random_labels=True, randomizes the labels. 
+	Inputs: train_batch_size (default: 100), test_batch_size (default:100), train_range (default: None), test_range (default: None), random_labels (default: False), seed (default: None)
+	Return: train dataset, test dataset, train loader, test loader
+	"""
+	normalize = transforms.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761])
+	transform_train = transforms.Compose([
+		transforms.ToTensor(),
+		normalize])
+	transform_test = transforms.Compose([
+		transforms.ToTensor(),
+		normalize])
+	train_dataset = datasets.CIFAR100(
+								root='data', 
+								train=True, 
+								transform=transform_train,
+								download=True)
+	test_dataset = datasets.CIFAR100(
+								root='data', 
+								train=False, 
+								transform=transform_test,
+								download=True)
+	if random_labels:
+		print("generating random labels with seed {}".format(seed))
+		np.random.seed(seed)
+
+		probability_of_random = 1.0
+		labels = np.array(train_dataset.targets) 
+		mask = np.random.rand(len(labels)) <= probability_of_random #create mask of length labels, where entries drawn from [0,1].
+		rnd_labels = np.random.choice(10, mask.sum())               #create random labels 1-10 of length of mask
+		labels[mask] = rnd_labels
+		labels = [int(x) for x in labels]
+		train_dataset.targets = labels                              #assign new random labels to dataset
+		np.savetxt("random_labels.txt", labels)
+
+	if train_range:
+		train_dataset = Subset(train_dataset, train_range)
+
+	if test_range:
+		test_dataset = Subset(test_dataset, test_range)
+
+
+	train_loader = DataLoader(
+		dataset=train_dataset, 
+		batch_size=train_batch_size,
+		num_workers=4,
+		shuffle=False)
+	test_loader = DataLoader(
+		dataset=test_dataset, 
+		batch_size=test_batch_size,
+		num_workers=4,
+		shuffle=False)
+	return train_dataset, test_dataset, train_loader, test_loader
