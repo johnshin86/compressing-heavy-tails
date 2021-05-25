408b8e7f43e8672eaacb7d10038ce54bef3d5dcf
John Shin <johnyoungshin@gmail.com>
2021-05-25 12:37:05 -0400

    fixed imports

--- compressing_example.py
+++ compressing_example.py
@@ -4,19 +4,12 @@ Due to the random nature of the compression, the compressed accuracy will
 likely not exactly match the paper. 
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-from torch.utils.data.dataset import Subset
 import pytorchcv
+import powerlaw
 from pytorchcv.model_provider import get_model as ptcv_get_model
-
-from torchvision import datasets
-from torchvision import transforms
-
-import matplotlib.pyplot as plt
 
 from compressing_ht import *
 
@@ -24,10 +17,11 @@ _,_, train_loader, test_loader = get_data_cifar10(train_batch_size=100, test_bat
 
 thismodel = 'densenet40_k12_cifar10'
 model = ptcv_get_model(thismodel, pretrained=True)
+model.to("cuda") #to make the compress_dense_compare function work faster
 
-data = model.output.weight.detach().numpy().flatten()
+data = model.output.weight.detach().cpu().numpy().flatten()
 
-a = fit_pl(model.output.weight.detach())
+a = fit_pl(model.output.weight.detach().cpu())
 
 pre, post = compress_dense_compare(model, test_loader, iterations=10)
 
